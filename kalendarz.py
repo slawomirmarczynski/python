68cb20476d6555c676c865737601d8baed13013e
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2021-05-09 13:08:02 +0200

    Signed-off-by: Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>

--- kalendarz.py
+++ kalendarz.py
@@ -10,7 +10,22 @@ for wpis in kalendarz.events:
     if wpis.begin <= teraz <= wpis.end:   
         print(wpis.name)
         print('=================')
+
+print()
+print('**************')
     
+# początek dnia 15 czerwca 2021, godzina 10:15    
+początek = arrow.get(2021, 6, 15, 10, 15)
+
+# koniec dnia 25 czerwca 2021, godzina 16:00
+koniec = arrow.get(2021, 6, 25, 16, 00)
+
+for wpis in kalendarz.events:
+    if ((początek <= wpis.begin and koniec >= wpis.begin)
+            or (koniec >= wpis.end and początek <= wpis.end)
+            or (początek >= wpis.begin) and (koniec <= wpis.end)):
+        print(wpis.name)
+        print('**************')
     
     
     
