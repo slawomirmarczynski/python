f5fde80b8a353aa4abad4ed4e6a4542aa64110a9
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2022-04-02 23:37:32 +0200

    Signed-off-by: Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>

--- data_structures/nested_list_print.py
+++ data_structures/nested_list_print.py
@@ -16,18 +16,32 @@ CC-BY-NC-ND 2022 Sławomir Marczyński
 # będące liczbami całkowitymi... a Python nie chce porównywać list i łańcuchów,
 # nie potrafi też porównać liczb i łańcuchów (czyli co jest większe 1 czy "1").
 
-L = ['Łańcuch znaków', ['Bob', [9, 8, 7]], ['Ala', [1, 2, 3]], 'albo', 4, []]
+L = ['Łańcuch znaków', ['Bob', [9, 8, 7]], ['Ala', [1, 2, 3]],
+     'albo', '',  4, []]
 
 def print_nice(data, ident=0):
+    """
+    Wypisywanie, ze wcięciami, list zagnieżdżonych.
+
+    Parametry:
+        data: co ma być wypisane.
+        ident: początkowa wartość wcięcia (domyślnie 0, czyli bez wcięcia).
+    """
 
     # Wielkość (w spacjach) wcięcia przy wypisywaniu kolejnego poziomu.
 
     PRINT_NICE_IDENT = 4
 
     def key_function(x):
+        """
+        Funkcja przyporządkująca kolejność elementów jako krotkę.
+
+        Parametry:
+            x: element.
+        """
         islist = isinstance(x, list)
         if islist:
-            x = x[0]
+            x = x[0] if x else None;
         for idx, ev in enumerate((int, str)):
             try:
                 value = ev(x)
@@ -36,24 +50,20 @@ def print_nice(data, ident=0):
                 pass
         return islist, -1, -1
 
-    if not data:
-        return
-
-    try:
-        data.remove('')
-    except:
-        pass
-
-    try:
-        data.remove([])
-    except:
-        pass
+    # Bierzemy kolejne elementy z posortowanej (na jednym poziomie) liście.
+    # Jeżeli taki element jest listą to rekurencyjnie wywołujemy print_nice,
+    # jeżeli listą nie jest to wypisujemy go.
 
     for element in sorted(data, key=key_function):
         if isinstance(element, list):
             print_nice(element, ident + PRINT_NICE_IDENT)
         else:
-            print(' '*ident, element, sep='')
+            # Warunek if poniżej sprawdza czy jest realnie coś do wypisania,
+            # czy też może element będzie łańcuchem pustym lub ciągiem spacji.
+            if len(str(element).strip()):
+                print(' '*ident, element, sep='')
 
+
+# Tu uruchamiamy napisaną funkcję dla listy L
 
 print_nice(L)
